88b69039a92bc63aa615a04e675300a03c214d1c
permlap-phola <permlap@tatugacamp.com>
2024-02-12 13:01:37 +0700

    Duplicate package json from farmer repository

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./index.html",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,8 +16,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "super-main-color": "#5C430D",
+        "main-color": "#597E52",
+        "secondary-color": "#3193A9",
+        "third-color": "#C6A969",
+        "fourth-color": "#FFFFEC",
+      },
+      fontFamily: {
+        Anuphan: ["Anuphan", "sans-serif"],
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-react-aria-components")({ prefix: "rac" }),
+    require("tailwindcss-animate"),
+  ],
 };
 export default config;
