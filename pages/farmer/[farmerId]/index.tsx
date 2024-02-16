400ef46fbdec954166d9fb46d986b224d8b1fe74
permlap-phola <permlap@tatugacamp.com>
2024-02-16 22:12:39 +0700

    Get Back Data

--- pages/farmer/[farmerId]/index.tsx
+++ pages/farmer/[farmerId]/index.tsx
@@ -1,14 +1,44 @@
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { parseCookies } from "nookies";
-import React from "react";
+import React, { useState } from "react";
 import { GetUserService } from "../../../services/user";
 import { User } from "../../../model";
 import DashboardLayout from "../../../layouts/dashboardLayout";
+import { MenuFarmerEvaluation } from "../../../data/menus";
+import MenuCardEvaluationFarmer from "../../../components/cards/menuCardEvaluationFarmer";
+import Kos01Form from "../../../components/forms/kos01Form";
+import Kos02Form from "../../../components/forms/kos02Form";
+import Kos03Form from "../../../components/forms/kos03Form";
+import Kos04Form from "../../../components/forms/kos04Form";
+import Kos06Form from "../../../components/forms/kos06Form";
+import Kos05Form from "../../../components/forms/kos05Form";
 
 function Index({ userServer }: { userServer: User }) {
+  const [selectMenu, setSelectMenu] = useState<number>(0);
   return (
     <DashboardLayout user={userServer}>
-      <header></header>
+      <div className="mt-10 flex w-full flex-col items-center justify-start gap-5">
+        <header className="grid h-40 w-11/12 grid-cols-6 place-items-center gap-3">
+          {MenuFarmerEvaluation.map((menu, index) => {
+            return (
+              <MenuCardEvaluationFarmer
+                setSelectMenu={setSelectMenu}
+                index={index}
+                card={menu}
+                key={index}
+              />
+            );
+          })}
+        </header>
+        <main>
+          {selectMenu === 0 && <Kos01Form />}
+          {selectMenu === 1 && <Kos02Form />}
+          {selectMenu === 2 && <Kos03Form />}
+          {selectMenu === 3 && <Kos04Form />}
+          {selectMenu === 4 && <Kos05Form />}
+          {selectMenu === 5 && <Kos06Form />}
+        </main>
+      </div>
     </DashboardLayout>
   );
 }
