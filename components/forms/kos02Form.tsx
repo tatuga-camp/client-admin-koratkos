ab05b3ed1561381c8cd3218eed6bc7ba834edc2a
permlap-phola <permlap@tatugacamp.com>
2024-02-17 18:14:07 +0700

    K0S01 - KOS05

--- components/forms/kos02Form.tsx
+++ components/forms/kos02Form.tsx
@@ -1,7 +1,119 @@
+import { UseQueryResult } from "@tanstack/react-query";
 import React from "react";
+import { ResponseGetFarmerKos02Service } from "../../services/farmer";
+import Image from "next/image";
+import { MdFileDownload, MdLandslide } from "react-icons/md";
+import { GiWeight } from "react-icons/gi";
+import { FaSeedling } from "react-icons/fa";
+import moment from "moment";
+import { backgroundImageBase64 } from "../../data/base64Images";
+type Kos02FormProps = {
+  kos02: UseQueryResult<ResponseGetFarmerKos02Service, Error>;
+};
+function Kos02Form({ kos02 }: Kos02FormProps) {
+  return (
+    <section className="flex h-max w-full flex-col items-center justify-start gap-5">
+      <section className=" grid h-max w-11/12 grid-cols-2 gap-2 rounded-lg bg-fourth-color p-5 xl:grid-cols-3">
+        <section className="flex h-max w-full flex-col items-center justify-center gap-2 rounded-lg p-5 ring-1 ring-black">
+          <h1 className="rounded-lg bg-fifth-color px-20 py-1 text-xl font-semibold text-white">
+            แผนผังแปลง
+          </h1>
+          <div className="relative h-96 w-full ">
+            <Image
+              src={kos02.data?.map as string}
+              fill
+              blurDataURL={backgroundImageBase64}
+              placeholder="blur"
+              className="object-contain"
+              alt="image map"
+            />
+          </div>
 
-function Kos02Form() {
-  return <div>Kos02Form</div>;
+          <a
+            href={kos02.data?.map as string}
+            download="map.jpg"
+            target="_blank"
+            className="flex w-max cursor-pointer items-center justify-center gap-2 rounded-lg bg-super-main-color
+           px-5 py-1 text-xl font-semibold text-white drop-shadow-md transition 
+           duration-100 hover:scale-105 active:scale-110"
+          >
+            <MdFileDownload />
+            ดาวน์โหลด
+          </a>
+        </section>
+        {kos02.data?.orgCropProdCalForKos2s?.map((orgCropProd, index) => {
+          const rangeDate = orgCropProd.rangeDate.map((time) => {
+            return ` ${moment(time).format("MMMM")} ,`;
+          });
+          const yearPlan = moment(orgCropProd.yearPlan).format("YYYY");
+          return (
+            <li
+              key={orgCropProd.id}
+              className="flex h-full w-full flex-col items-center justify-start gap-1 rounded-lg bg-[#F1E4C3] p-4"
+            >
+              <section className="flex w-full justify-between ">
+                <h2
+                  className="flex items-center justify-center rounded-lg bg-[#502D16]
+              px-3 py-1 text-xl font-semibold text-white"
+                >
+                  แปลงที่ {orgCropProd.plotNumber}
+                </h2>
+              </section>
+              <section className="mt-2 flex w-full flex-col items-start">
+                <h1 className="text-4xl font-semibold text-super-main-color">
+                  {orgCropProd.plantType}
+                </h1>
+                <span className="text-blue-600">
+                  แหล่งที่มา: {orgCropProd.source}
+                </span>
+              </section>
+              <section className="mt-5 flex w-full justify-center gap-2">
+                <div className="flex w-full items-center justify-center gap-2">
+                  <div className="rounded-full bg-super-main-color p-2 text-lg text-white">
+                    <MdLandslide />
+                  </div>
+                  <span className="font-semibold">
+                    พื้นที่ {orgCropProd.landArea} ไร่
+                  </span>
+                </div>
+                <div className="flex w-full items-center justify-center gap-2">
+                  <div className="rounded-full bg-super-main-color p-2 text-lg text-white">
+                    <GiWeight />
+                  </div>
+                  <div className="text-xs font-semibold">
+                    <span className="text-sm">
+                      ผลผลิต {orgCropProd.yieldPerRai.toLocaleString()}{" "}
+                    </span>
+                    <div className="text-xs">กก./ไร่</div>
+                  </div>
+                </div>
+              </section>
+              <section className="mt-5 flex flex-col items-center justify-center gap-2 border-b-2 border-[#502D16]">
+                <div className="rounded-full bg-super-main-color p-3 text-3xl text-white">
+                  <FaSeedling />
+                </div>
+                <h1 className="text-2xl font-semibold text-[#502D16]">
+                  {orgCropProd.seed}
+                </h1>
+              </section>
+              <section className="mt-5 flex w-full flex-col items-center justify-center gap-3">
+                <span className="font-bold text-[#502D16]">
+                  เดือน:{" "}
+                  <span className="text-super-main-color ">{rangeDate}</span>
+                </span>
+                <span className="font-bold text-[#502D16]">
+                  แผนการผลิตพืชอินทรีย์ ประจำปีการผลิต
+                </span>
+                <span className="text-xl font-bold text-super-main-color">
+                  {yearPlan}
+                </span>
+              </section>
+            </li>
+          );
+        })}
+      </section>
+    </section>
+  );
 }
 
 export default Kos02Form;
--- components/forms/kos03Form.tsx
+++ components/forms/kos03Form.tsx
@@ -1,7 +1,105 @@
+import { UseQueryResult } from "@tanstack/react-query";
 import React from "react";
+import { ResponseGetFarmerKos03Service } from "../../services/farmer";
+import moment from "moment";
+import Image from "next/image";
+import { backgroundImageBase64 } from "../../data/base64Images";
 
-function Kos03Form() {
-  return <div>Kos03Form</div>;
+type Kos03FormProps = {
+  kos03: UseQueryResult<ResponseGetFarmerKos03Service, Error>;
+};
+function Kos03Form({ kos03 }: Kos03FormProps) {
+  return (
+    <main className="grid w-11/12 grid-cols-2 gap-10 rounded-lg bg-fourth-color p-5 xl:grid-cols-3">
+      {kos03.data?.activities.map((activity) => {
+        const activityDate = moment(activity.activityDate)
+          .locale("th")
+          .format("DD/MMMM/YYYY");
+        const image = activity.files.length > 0 ? activity.files[0].url : "";
+        return (
+          <section
+            className="flex  h-max w-full flex-col gap-4 bg-[#F1E4C3] p-4 py-5 font-Anuphan lg:rounded-lg lg:drop-shadow-md"
+            key={activity.id}
+          >
+            <div className="flex w-full flex-row-reverse justify-between">
+              <div
+                className={` w-max flex-col ${image ? "hidden" : "flex px-6 py-2"}  items-start justify-between
+             rounded-lg bg-[#597E52] `}
+              >
+                <h1 className="text-2xl font-semibold text-white">
+                  แปลงที่ {activity.plotNumber}
+                </h1>
+                <h2 className="text-base font-medium text-white">
+                  {activityDate}
+                </h2>
+              </div>
+            </div>
+            {image && (
+              <div className="relative h-60 w-full overflow-hidden rounded-lg bg-slate-300">
+                <Image
+                  src={image}
+                  placeholder="blur"
+                  blurDataURL={backgroundImageBase64}
+                  alt="image of activity"
+                  fill
+                  className="object-cover"
+                />
+                <div
+                  className={` w-max flex-col ${image ? "flex px-4 py-2" : "hidden"} absolute bottom-2 right-2 z-40 m-auto  items-start justify-between
+             rounded-lg bg-[#597E52] `}
+                >
+                  <h1 className="text-xl font-semibold text-white">
+                    แปลงที่ {activity.plotNumber}
+                  </h1>
+                  <h2 className="text-sm font-medium text-white">
+                    {activityDate}
+                  </h2>
+                </div>
+              </div>
+            )}
+            <h3 className="text-lg font-semibold text-super-main-color underline">
+              รายละเอียด
+            </h3>
+            {kos03.isFetching ? (
+              <div className="bg flex h-max w-full flex-col gap-2">
+                {[...new Array(4)].map((_, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="h-3 w-full animate-pulse bg-slate-300"
+                    ></div>
+                  );
+                })}
+              </div>
+            ) : (
+              <div className=" text-pretty indent-5 text-sm font-medium text-super-main-color">
+                {activity.note}
+              </div>
+            )}
+            <div className="grid w-full grid-cols-3 gap-5">
+              {activity.files.map((file) => {
+                return (
+                  <a
+                    href={file.url}
+                    target="_blank"
+                    className="relative h-20  w-20 overflow-hidden rounded-lg bg-slate-500"
+                    key={file.id}
+                  >
+                    <Image
+                      src={file.url}
+                      alt="image of activity"
+                      fill
+                      className="object-cover transition duration-100 hover:scale-110"
+                    />
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </main>
+  );
 }
 
 export default Kos03Form;
