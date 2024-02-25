b22b488d288f792542a1849e3f65176eca715d13
permlap-phola <permlap@tatugacamp.com>
2024-02-26 01:36:57 +0700

    Change Download File Concept

--- services/dowloadFile.ts
+++ services/dowloadFile.ts
@@ -5,60 +5,39 @@ type RequestDowloadPDFService = {
   farmerId: string;
 };
 export async function DowloadPDFService(input: RequestDowloadPDFService) {
-  const cookies = parseCookies();
-  const access_token = cookies.access_token;
-  const html = await axios({
-    method: "GET",
-    url: `${process.env.NEXT_PUBLIC_SERVER_URL}/file-generate/download-pdf`,
-    params: {
-      ...input,
-    },
-    responseType: "blob",
-    headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
-  })
-    .then((response) => {
-      downloadFile(response.data, "download.pdf", "application/pdf");
-    })
-    .catch(async (error) => {
-      // Check if the error response is a blob and its type is 'application/json'
-      if (
-        error.response?.data instanceof Blob &&
-        error.response.data.type === "application/json"
-      ) {
-        try {
-          // Convert the blob to text
-          const errorText = await error.response.data.text();
-          // Parse the text as JSON
-          const errorJson = JSON.parse(errorText);
-          // Log the error message
-          console.error(errorJson);
-          // You might want to throw a more readable error or the parsed JSON
-          throw errorJson;
-        } catch (parseError: any) {
-          // If there's an error parsing the blob as text or parsing the text as JSON
-          console.error("Error parsing error response:", parseError);
-          // Throw the original error or a custom error message
-          throw parseError;
-        }
-      } else {
-        // If the response is not a blob or not 'application/json', log the original error
-        console.error(error.response?.data || error);
-        // Throw the original error or a custom error message
-        throw error?.response?.data || new Error("An unknown error occurred.");
-      }
+  try {
+    const cookies = parseCookies();
+    const access_token = cookies.access_token;
+    const urlPDF = await axios<string>({
+      method: "GET",
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/file-generate/download-pdf`,
+      params: {
+        ...input,
+      },
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
     });
+
+    await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        downloadFile(urlPDF.data, "file.pdf");
+        resolve();
+      }, 5000);
+    });
+  } catch (error: any) {
+    console.error(error.response.data);
+    throw error?.response?.data;
+  }
 }
 
 // Function to trigger the file download
-function downloadFile(data: Blob, filename: string, mimeType: string): void {
-  const blob = new Blob([data], { type: mimeType });
-  const url = window.URL.createObjectURL(blob);
+function downloadFile(url: string, fileName: string): void {
   const a = document.createElement("a");
   a.href = url;
-  a.download = filename;
+  a.download = fileName;
   document.body.appendChild(a);
+  a.target = "_blank";
   a.click();
   window.URL.revokeObjectURL(url);
   document.body.removeChild(a);
